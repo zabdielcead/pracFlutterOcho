d7888e4e6715a37673ec6831eb2b3c7440a5a33a
zabdielcead <carteagad@ruv.org.mx>
2020-06-17 13:53:30 -0500

    primer commit

---
+++ formvalidation/lib/src/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
+class HomePage extends StatelessWidget {
+  
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home')        
+      ),
+      body: Container(),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon( Icons.add ),
+      backgroundColor: Colors.deepPurple,
+      onPressed: () => Navigator.pushNamed(context, 'producto'),
+    );
+  }
+}
---
+++ formvalidation/lib/src/pages/producto_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
+class ProductoPage extends StatefulWidget {
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+
+}  
+  
+ 
+
+
+class _ProductoPageState extends State<ProductoPage>{
+  final formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Producto'),
+        actions: <Widget>[
+          IconButton(
+                icon: Icon( Icons.photo_size_select_actual ), 
+                onPressed: (){}
+          ),
+           IconButton(
+                icon: Icon( Icons.camera_alt ), 
+                onPressed: (){}
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: <Widget>[
+                _crearNombre(),
+                _crearPrecio(),
+                _crearBoton()
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget _crearNombre() {
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto'
+      ) ,
+      validator: (value) {
+          if( value.length < 3 ){
+            return 'Ingrese el nombre del producto';
+          }else{
+            return null;
+          }
+      },
+    );
+  }
+
+  Widget _crearPrecio() {
+    return TextFormField(
+      keyboardType:  TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: 'Precio'
+      ) ,
+      validator: (value){
+        if( utils.isNumeric(value)  ){
+            return null;
+        }else{
+            return 'Solo numeros';
+        }
+      },
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton.icon(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      ),
+                      textColor: Colors.white,
+                      color: Colors.deepPurple,
+                      label: Text('Guardar'),
+                      onPressed: _submit, 
+                      icon: Icon(Icons.save), 
+                    );
+  }
+
+  void _submit() {
+
+    if(!formkey.currentState.validate()) return;
+    print('Todo OK');
+  }
+
+  
+}
